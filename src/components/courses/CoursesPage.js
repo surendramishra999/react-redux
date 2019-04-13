8cdce1f12600ce1f780c35636a76ebdb2755acc7
root <root@surend3145420-2.Nagarro.local>
2019-04-14 00:00:58 +0530

    Redux component added for courses with boilerplate

--- src/components/courses/CoursesPage.js
+++ src/components/courses/CoursesPage.js
@@ -1,13 +1,51 @@
 import React from "react";
+import { connect } from "react-redux";
+import * as courseActions from "./../../redux/actions/CourseAction";
+import PropTypes from "prop-types";
 
 class CoursePage extends React.Component {
+  state = {
+    course: {
+      title: ""
+    }
+  };
+
+  handleChange = event => {
+    const course = { ...this.state.course, title: event.target.value };
+    this.setState({ course });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+    this.props.dispatch(courseActions.createCourse(this.state.course));
+  };
   render() {
     return (
-      <div>
-        <h1>Courses</h1>
-      </div>
+      <form onSubmit={this.handleSubmit}>
+        <h2>Courses</h2>
+        <input
+          type="text"
+          onChange={this.handleChange}
+          value={this.state.course.title}
+        />
+        <input type="submit" value="save" />
+        {this.props.courses.map(course => (
+          <div key={course.title}>{course.title}</div>
+        ))}
+      </form>
     );
   }
 }
 
-export default CoursePage;
+CoursePage.propTypes = {
+  dispatch: PropTypes.func.isRequired,
+  courses: PropTypes.array.isRequired
+};
+
+function mapStateToProps(state) {
+  return {
+    courses: state.courses
+  };
+}
+
+export default connect(mapStateToProps)(CoursePage);
--- src/index.js
+++ src/index.js
@@ -3,10 +3,15 @@ import { render } from "react-dom";
 import { BrowserRouter as Router } from "react-router-dom";
 import "bootstrap/dist/css/bootstrap.min.css";
 import App from "./components/App";
+import configureStore from "./redux/ConfigureStore";
+import { Provider as ReduxProvider } from "react-redux";
 
+const store = configureStore();
 render(
-  <Router>
-    <App />
-  </Router>,
+  <ReduxProvider store={store}>
+    <Router>
+      <App />
+    </Router>
+  </ReduxProvider>,
   document.getElementById("app")
 );
