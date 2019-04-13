e7d3e986a5efe146829057d8be158733b1e5e7da
root <root@surend3145420-2.Nagarro.local>
2019-04-13 14:21:32 +0530

    Npm scripts added

--- webpack.config.dev.js
+++ webpack.config.dev.js
@@ -7,7 +7,7 @@ process.env.NODE_ENV = "development";
 module.exports = {
   mode: "development",
   target: "web",
-  devTool: "cheap-module-source-map",
+  devtool: "cheap-module-source-map",
   entry: "./src/index",
   output: {
     path: path.resolve(__dirname, "build"),
