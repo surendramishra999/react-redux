8c80186e45b190a0461d43d235a298544098b4ce
root <root@surend3145420-2.Nagarro.local>
2019-04-14 14:46:53 +0530

    Class component Manage Coures page added

---
+++ src/components/courses/ManageCoursePage.js
@@ -0,0 +1,52 @@
+import React from "react";
+import { connect } from "react-redux";
+import * as courseActions from "./../../redux/actions/CourseAction";
+import * as authorActions from "./../../redux/actions/AuthorAction";
+import PropTypes from "prop-types";
+
+class ManageCoursePage extends React.Component {
+  componentDidMount() {
+    const { courses, authors, loadAuthors, loadCourses } = this.props;
+    if (courses.length === 0) {
+      loadCourses().catch(error => {
+        alert("Loading courses failed.." + error);
+      });
+    }
+
+    if (authors.length === 0) {
+      loadAuthors().catch(error => {
+        alert("Loading authors failed.." + error);
+      });
+    }
+  }
+  render() {
+    return (
+      <>
+        <h2>Manage Course</h2>
+      </>
+    );
+  }
+}
+
+ManageCoursePage.propTypes = {
+  courses: PropTypes.array.isRequired,
+  authors: PropTypes.array.isRequired,
+  loadCourses: PropTypes.func.isRequired,
+  loadAuthors: PropTypes.func.isRequired
+};
+function mapStateToProps(state) {
+  return {
+    courses: state.courses,
+    authors: state.authors
+  };
+}
+
+const mapDispatchToProps = {
+  loadCourses: courseActions.loadCourses,
+  loadAuthors: authorActions.loadAuthors
+};
+
+export default connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(ManageCoursePage);
