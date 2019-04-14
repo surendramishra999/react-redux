eeb31bc2667710dbb57292cf60d42214a5375f2e
root <root@surend3145420-2.Nagarro.local>
2019-04-14 14:53:33 +0530

    Manage courses page: changed into functional component using Hook 'UseEffect'

--- src/components/courses/ManageCoursePage.js
+++ src/components/courses/ManageCoursePage.js
@@ -1,12 +1,11 @@
-import React from "react";
+import React, { useEffect } from "react";
 import { connect } from "react-redux";
 import * as courseActions from "./../../redux/actions/CourseAction";
 import * as authorActions from "./../../redux/actions/AuthorAction";
 import PropTypes from "prop-types";
 
-class ManageCoursePage extends React.Component {
-  componentDidMount() {
-    const { courses, authors, loadAuthors, loadCourses } = this.props;
+function ManageCoursePage({ courses, authors, loadAuthors, loadCourses }) {
+  useEffect(() => {
     if (courses.length === 0) {
       loadCourses().catch(error => {
         alert("Loading courses failed.." + error);
@@ -18,14 +17,12 @@ class ManageCoursePage extends React.Component {
         alert("Loading authors failed.." + error);
       });
     }
-  }
-  render() {
-    return (
-      <>
-        <h2>Manage Course</h2>
-      </>
-    );
-  }
+  }, []);
+  return (
+    <>
+      <h2>Manage Course</h2>
+    </>
+  );
 }
 
 ManageCoursePage.propTypes = {
